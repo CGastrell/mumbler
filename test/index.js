4294651a1b8f2fcdc6b7287c30a263553222ec10
Christian Gastrell <cgastrell@gmail.com>
2017-08-06 18:51:53 -0300

    initial commit 2

---
+++ test/index.js
@@ -0,0 +1,13 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var mumbler = require('../')
+
+// attach to some endpoint
+app.get('/mumbler', mumbler(http));
+
+http.listen(3000, function(){
+  console.log('Open http://localhost:3000/mumbler on your browser')
+});
+
+// spit some output constantly so we can check output
+setInterval(function(){console.log(new Date())}, 1000)
