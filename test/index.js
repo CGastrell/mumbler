9ed5e45dcb50ddc64a70f4510faad0e85408311f
Christian Gastrell <cgastrell@gmail.com>
2017-08-06 21:04:49 -0300

    code sugar

--- test/index.js
+++ test/index.js
@@ -1,13 +1,16 @@
+var mumbler = require('../')
 var app = require('express')();
 var http = require('http').Server(app);
-var mumbler = require('../')
 
 // attach to some endpoint
 app.get('/mumbler', mumbler(http));
+
 
 http.listen(3000, function(){
   console.log('Open http://localhost:3000/mumbler on your browser')
 });
 
 // spit some output constantly so we can check output
-setInterval(function(){console.log(new Date())}, 1000)
+setInterval(function(){
+  console.log(new Date())
+}, 1000)
